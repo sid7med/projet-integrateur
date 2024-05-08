e9797fe9abee0fe70fd469c1ac7e399b60127921
zeiny <23025@supnum.mr>
2024-05-08 01:29:03 +0000

    la deriner  vertion du base de donner et linsetion du stage par le prof

--- PROJET INTEGRATEUR/ins.php
+++ PROJET INTEGRATEUR/ins.php
@@ -2,21 +2,35 @@
 include "db_conn.php";
 
 if (isset($_POST['submit'])) {
-    $titre = $_POST['titer'];
-    $Encadrer = $_POST['Encadrer'];
-    $Description = $_POST['Description'];
-    $semester = $_POST['semester'];
-    $Technologie = $_POST['Technologie'];
-    
-    $sql = "INSERT INTO p_i (titre, encadrer, `description`, technologie, semester) 
-            VALUES ('$titre', '$Encadrer', '$Description', '$Technologie', '$semester')";
 
-    if (mysqli_query($conn, $sql)) {
-        echo "Inserted successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    $titre = mysqli_real_escape_string($conn, $_POST['titer']);
+    $Encadrer = mysqli_real_escape_string($conn, $_POST['Encadrer']);
+    $Description = mysqli_real_escape_string($conn, $_POST['Description']);
+    $semester = mysqli_real_escape_string($conn, $_POST['semester']);
+    $Technologie = mysqli_real_escape_string($conn, $_POST['Technologie']);
+    $annee = mysqli_real_escape_string($conn, $_POST['annee']);
+    $professors = $_POST['professors'];
+
+    $lastInsertedId = null;
+    $professorId = null;
+
+    foreach($professors as $prof) {
+        $sql = "INSERT INTO p_i (titre, encadrer, `description`, technologie, semester, id_professeur, annee)
+                VALUES ('$titre', '$Encadrer', '$Description', '$Technologie', '$semester', '$prof', '$annee')";
+
+        if (mysqli_query($conn, $sql)) {
+            $lastInsertedId = mysqli_insert_id($conn);
+            $professorId = $prof;
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
-    
+
+    $sql1 = "INSERT INTO encadrant(id_professeur, id_pi) VALUES ('$professorId', '$lastInsertedId')";
+if (mysqli_query($conn,$sql1))
+{
+    echo"yes";
+}
     mysqli_close($conn);
 }
 ?>
